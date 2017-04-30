66edb209bc544d62e4214b2eb35c16891d781187
Keith Orlando <korlando@mit.edu>
2017-04-30 14:53:24 -0400

    add initial pug files, move app to home.pug

---
+++ routes/pages.js
@@ -0,0 +1,27 @@
+// map page pathname to metadata
+module.exports = {
+  'home': {
+    title: 'Potential Flow Graphing App'
+  },
+  'about': {
+    title: 'About - Potential Flow'
+  },
+  'story': {
+    title: 'The Story of Potential Flow - Potential Flow'
+  },
+  'definitions': {
+
+  },
+  'derivations': {
+
+  },
+  'faq': {
+    title: 'FAQ - Potential Flow'
+  },
+  'equations': {
+    title: 'Equation Sheet - Potential Flow'
+  },
+  'references': {
+
+  }
+};
--- routes/routes.js
+++ routes/routes.js
@@ -1,4 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
+const pages = require('./pages');
+
+router.get('/', (req, res, next) => {
+  res.render('home', pages.home);
+  /*res.sendFile(path.resolve(__dirname, '../www/index.html'), {}, (err) => {
+    if(err) {
+      next(err);
+    }
+  });*/
+});
 
 module.exports = router;
