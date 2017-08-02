c771bbb070eb0a4049872ea23919e4e0d722dae9
Keith Orlando <korlando@mit.edu>
2017-08-02 00:08:59 -0400

    update flow-elements page with corner flow

--- src/constants/variableMeta.js
+++ src/constants/variableMeta.js
@@ -4,42 +4,42 @@ import Naught from '../components/Naught';
 export default {
   U: {
     name: 'U',
-    placeholder: 'x strength'
+    placeholder: 'X strength'
   },
   V: {
     name: 'V',
-    placeholder: 'y strength'
+    placeholder: 'Y strength'
   },
   x0: {
     name: <Naught symbol="x"/>,
-    placeholder: 'x position'
+    placeholder: 'X position'
   },
   y0: {
     name: <Naught symbol="y"/>,
-    placeholder: 'y position'
+    placeholder: 'Y position'
   },
   theta0: {
     name: <Naught symbol={<span>&theta;</span>}/>,
-    placeholder: 'offset angle',
+    placeholder: 'Orientation',
   },
   m: {
     name: 'm',
-    placeholder: 'magnitude'
+    placeholder: 'Magnitude'
   },
   mu: {
     name: <span>&mu;</span>,
-    placeholder: 'magnitude'
+    placeholder: 'Magnitude'
   },
   gamma: {
     name: <span>&Gamma;</span>,
-    placeholder: 'magnitude'
+    placeholder: 'Magnitude'
   },
   alpha: {
     name: <span>&alpha;</span>,
-    placeholder: 'angle'
+    placeholder: 'Angle'
   },
   beta: {
     name: <span>&beta;</span>,
-    placeholder: 'magnitude',
+    placeholder: 'Magnitude',
   },
 };
